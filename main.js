2629a7bc7f903ab04289e723536f0aae11ad89d6
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2022-01-05 15:08:06 +0100

    added a puppeteer scraper which reads maxbo.no for stock
    currently prints instock/not in stock for wood in oslo. I am freezing!

---
+++ main.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+
+const findAndWaitFor = async (page, selector) => {
+    await page.waitForSelector(selector);
+    return await page.$(selector);
+}
+
+const findAndWaitForXpath = async (page, xpath) => {
+    await page.waitForXPath(xpath);
+    return await page.$x(xpath);
+
+}
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://www.maxbo.no/bjorkeved-15-kg-ca-40l-p2259774/');
+
+    const cookieButton = await findAndWaitFor(page, '[aria-label="Godkjenn alle"]')
+    await cookieButton.click();
+
+    const [availabilityButton] = await findAndWaitForXpath(page, "//button[contains(., 'butikker')]");
+    await availabilityButton.click();
+
+    await page.waitForSelector("[city=Oslo]");
+
+    const stores = await page.$$eval("[city=Oslo]", els => els.map(el => el.innerHTML));
+    const inStock = stores.filter(s => !s.includes("Ikke på lager"))
+    if (inStock.length > 0) {
+        console.log("In stock:")
+        console.log(inStock)
+    }
+    else {
+        console.log("nothing in stock")
+    }
+    await browser.close();
+})();
