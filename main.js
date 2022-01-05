dd89005896fa836c7b8ee340e0c29957b426c892
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2022-01-05 20:38:17 +0100

    temp5

--- main.js
+++ main.js
@@ -36,7 +36,8 @@ const findAndWaitForXpath = async (page, xpath) => {
   const inStock = stores.filter((s) => !s.includes("Ikke på lager"));
   if (inStock.length > 0) {
     const outfile = process.env.GITHUB_ENV;
-    fs.writeFileSync(outfile, `IN_STOCK=${inStock}`);
+    console.log(`writing output to ${outfile}`);
+    fs.appendFileSync(outfile, `actionState=instock`);
     console.log("in stock");
     console.log(inStock);
   } else {
