388b242059858ba542cd6fdc32d6fd3e8921c147
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2022-01-05 20:23:19 +0100

    added github action and write output to file to next step

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 const findAndWaitFor = async (page, selector) => {
-    await page.waitForSelector(selector);
-    return await page.$(selector);
-}
+  await page.waitForSelector(selector);
+  return await page.$(selector);
+};
 
 const findAndWaitForXpath = async (page, xpath) => {
-    await page.waitForXPath(xpath);
-    return await page.$x(xpath);
-
-}
+  await page.waitForXPath(xpath);
+  return await page.$x(xpath);
+};
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://www.maxbo.no/bjorkeved-15-kg-ca-40l-p2259774/');
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://www.maxbo.no/bjorkeved-15-kg-ca-40l-p2259774/");
 
-    const cookieButton = await findAndWaitFor(page, '[aria-label="Godkjenn alle"]')
-    await cookieButton.click();
+  const cookieButton = await findAndWaitFor(
+    page,
+    '[aria-label="Godkjenn alle"]'
+  );
+  await cookieButton.click();
 
-    const [availabilityButton] = await findAndWaitForXpath(page, "//button[contains(., 'butikker')]");
-    await availabilityButton.click();
+  const [availabilityButton] = await findAndWaitForXpath(
+    page,
+    "//button[contains(., 'butikker')]"
+  );
+  await availabilityButton.click();
 
-    await page.waitForSelector("[city=Oslo]");
+  await page.waitForSelector("[city=Oslo]");
 
-    const stores = await page.$$eval("[city=Oslo]", els => els.map(el => el.innerHTML));
-    const inStock = stores.filter(s => !s.includes("Ikke på lager"))
-    if (inStock.length > 0) {
-        console.log("In stock:")
-        console.log(inStock)
-    }
-    else {
-        console.log("nothing in stock")
-    }
-    await browser.close();
+  const stores = await page.$$eval("[city=Oslo]", (els) =>
+    els.map((el) => el.innerHTML)
+  );
+  const inStock = stores.filter((s) => !s.includes("Ikke på lager"));
+  if (inStock.length > 0) {
+    const outfile = process.env.GITHUB_ENV;
+    fs.writeFileSync(outfile, "inStock");
+    console.log("in stock");
+    console.log(inStock);
+  } else {
+    console.log("nothing in stock");
+  }
+  await browser.close();
 })();
